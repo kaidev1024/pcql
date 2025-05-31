2b04c798cb3e09886ff126cbed086db1c2e7f93c
kaidev1024 <58545754+kaidev1024@users.noreply.github.com>
2025-05-31 14:50:55 -0700

    minor fix

--- table.go
+++ table.go
@@ -2,12 +2,11 @@ package pcql
 
 import "github.com/scylladb/gocqlx/v3/table"
 
-func CreateTable(name string, partKeys, sortKeys, otherCols []string) *table.Table {
-	m := table.Metadata{
+func CreateTable(name string, partKeys, sortKeys, columns []string) *table.Table {
+	return table.New(table.Metadata{
 		Name:    name,
 		PartKey: partKeys,
 		SortKey: sortKeys,
-		Columns: append(append(partKeys, sortKeys...), otherCols...),
-	}
-	return table.New(m)
+		Columns: columns,
+	})
 }
